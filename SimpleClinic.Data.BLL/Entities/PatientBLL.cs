1077af31e1a8dbbb5a6d91434b0885e200b21210
MalikBNB <167820077+MalikBNB@users.noreply.github.com>
2024-07-11 17:32:04 +0100

    Create MainWindow and PatientUc

---
+++ SimpleClinic.Data.BLL/Entities/PatientBLL.cs
@@ -0,0 +1,63 @@
+﻿using SimpleClinic.Data.Layers;
+using SimpleClinic.Data.Layers.EntitesInfos;
+using SimpleClinic.Data.Layers.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClinic.Data.Bll.Entities
+{
+    public class PatientBLL : SharedBll
+    {
+        public async static void Add(AppDbContext db, Patient patient)
+        {
+            db.Patients.Add(patient);
+            await db.SaveChangesAsync();
+        }
+
+        public async static void Delete(int Id)
+        {
+            using (var db = new AppDbContext())
+            {
+                var patient = db.Patients.FirstOrDefault(x => x.Id == Id);
+                db.Patients.Remove(patient);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        //public async static Task<Patient> GetPatientById(int Id)
+        //{
+        //    return await Db.Patients.FirstOrDefaultAsync(p => p.Id == Id);    
+        //}
+
+        public  static Patient GetPatientById(int Id)
+        {
+            return  Db.Patients.Include(p => p.Person).FirstOrDefault(p => p.Id == Id);
+        }
+
+        public async static Task<List<PatientInfo>> GetAllPatients()
+        {
+            return await Db.Patients.Select(item => new PatientInfo
+            {
+                Id = item.Id,
+                PersonId = item.PersonId,
+                FirstName = item.Person.FirstName,
+                LastName = item.Person.LastName,
+                DateOfBirth = item.Person.DateOfBirth,
+                Gendor = item.Person.Gendor,
+                PhoneNo = item.Person.PhoneNo,
+                Email = item.Person.Email,
+                Address = item.Person.Address,
+                CreatorId = item.CreatorId,
+                Creator = item.Creator.Username,
+                Created = item.Created,
+                Modified = item.Modified,
+                Modifier = item.Modifier.Username,
+                ModifierId = item.ModifierId,
+            }).ToListAsync();
+        }
+    }
+}
---
+++ SimpleClinic.Shell/Utils/Icons.cs
@@ -0,0 +1,135 @@
+﻿
+
+namespace SimpleClinic.Shell.Utils
+{
+    public class Icons
+    {
+        public const string Appoinments16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/Appoinments16Px.png";
+        public const string Appoinment24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/Appoinment24Px.png";
+
+        public const string Calendar16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/Calendar16Px.png";
+        public const string Calendar24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/Calendar24Px.png";
+
+        public const string People16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/People16Px.png";
+        public const string People24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/People16Px.png";
+
+        public const string Doctor16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/Doctor16Px.png";
+        public const string Doctor24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/Doctor16Px.png";
+
+        public const string Patient16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/Patient16Px.png";
+        public const string Patient24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/Patient16Px.png";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public const string New16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/add_16px.png";
+
+        public const string New32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/add_32px.png";
+
+        public const string Edit16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/edit_16px.png";
+
+        public const string Edit32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/edit_32px.png";
+
+        public const string DeleteBin16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/delete_bin_16px.png";
+
+        public const string DeleteBin32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/delete_bin_32px.png";
+
+        public const string Cancel16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/cancel_16px.png";
+
+        public const string Cancel24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/cancel_24px.png";
+
+        public const string Cancel32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/cancel_32px.png";
+
+        public const string Save16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/save_16px.png";
+
+        public const string Save24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/save_24px.png";
+
+        public const string Save32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/save_32px.png";
+
+        public const string SaveAndClose16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/save_close_16px.png";
+
+        public const string SaveAndClose32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/save_close_32px.png";
+
+        public const string SaveAndNew16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/save_as_16px.png";
+
+        public const string SaveAndNew32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/save_as_32px.png";
+
+        public const string Export16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/microsoft_excel_2019_16px.png";
+
+        public const string Export32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/microsoft_excel_2019_32px.png";
+
+        public const string GridLayout16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/grid_template_16px.png";
+
+        public const string GridLayout32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/grid_template_32px.png";
+
+        public const string Import16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/add_list_16px.png";
+
+        public const string Import32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/add_list_32pxa.png";
+
+        public const string FromFileCsv16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/microsoft_excel_2019_16px.png";
+
+        public const string FromFileCsv32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/microsoft_excel_2019_32px.png";
+
+        public const string GridFilter16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/filter_16px.png";
+
+        public const string GridFilter24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/filter_24px.png";
+
+        public const string GridFilter32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/filter_32px.png";
+
+        public const string AddFilter16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/addFilter_16px.png";
+
+        public const string AddFilter24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/addFilter_24px.png";
+
+        public const string AddFilter32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/addFilter_32px.png";
+
+        public const string GridEditorFilter16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/editorFilter_16px.png";
+
+        public const string GridEditorFilter24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/editorFilter_24px.png";
+
+        public const string GridEditorFilter32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/editorFilter_32px.png";
+
+        public const string GridDeleteFilter16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/deleteFilter_16px.png";
+
+        public const string GridDeleteFilter24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/deleteFilter_24px.png";
+
+        public const string GridDeleteFilter32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/deleteFilter_32px.png";
+
+        public const string Forward10Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/forward_10px.png";
+
+        public const string Copy16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/copy_16px.png";
+
+        public const string Copy24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/copy_24px.png";
+
+        public const string Copy32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/copy_32px.png";
+
+        public const string EmptyFields16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/pencil_eraser_16px.png";
+
+        public const string EmptyFields32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/pencil_eraser_32px.png";
+
+        public const string Refresh16Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/refresh_16px.png";
+
+        public const string Refresh32Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/refresh_32px.png";
+
+        public const string Star24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/star_24px.png";
+
+        public const string DiStar24Px = "pack://application:,,,/SimpleClinic.Shell;component/Icons/diStar_24px.png";
+    }
+}
